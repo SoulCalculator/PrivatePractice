5f39f29c8065b0b5a407b5a5deef62d3bb31cfa6
LXRadiator <sjzezdean@gmail.com>
2015-05-01 11:05:22 -0400

    Roman To Integer written by vi

---
+++ FunctionTests/RomanToInteger.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int RomanToInt( string );
+
+int main (int argc, char* argv[])
+{
+	string test = "DCXXI";
+	int result = RomanToInt(test);
+	cout << result << endl;
+	return 0;
+}
+
+int RomanToInt( string str )
+{
+	int result = 0;
+	int value = 0;
+	int pre_value = 0;
+	for ( int i = 0; i < str.length(); i++ )
+	{
+		switch(str[i])
+		{
+			case 'I':
+			value = 1;
+			break;
+			case 'V':
+			value = 5;
+			break;
+			case 'X':
+			value = 10;
+			break;
+			case 'L':
+			value = 50;
+			break;
+			case 'C':
+			value = 100;
+			break;
+			case 'D':
+			value = 500;
+			break;
+			case 'M':
+			value = 1000;
+			break;
+			default:
+			value = 0;
+			break;
+		}
+		if (value > pre_value)
+		{
+			result += (value - 2 * pre_value);
+		}
+		else
+		{
+			result += value;
+		}
+		pre_value = value;
+	}
+	return result;
+}
